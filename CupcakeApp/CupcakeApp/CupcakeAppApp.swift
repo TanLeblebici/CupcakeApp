12ac89b0349a912bf4e57cced0a6774815564b94
TanLeblebici <75142903+TanLeblebici@users.noreply.github.com>
2022-08-23 22:32:14 +0300

    Add files via upload

---
+++ CupcakeApp/CupcakeApp/CupcakeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeAppApp.swift
+//  CupcakeApp
+//
+//  Created by Tan Leblebici on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeApp/CupcakeApp/Order.swift
@@ -0,0 +1,100 @@
+//
+//  Order.swift
+//  CupcakeApp
+//
+//  Created by Tan Leblebici on 20.08.2022.
+//
+
+import SwiftUI
+
+// a single class that stores all our data, which will be passed from screen to screen
+// This means all screens in our app share the same data which will work really well
+   
+class Order: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
+    static let types = ["Vailla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    var cost: Double {
+        // 2$ per cake
+        var cost = Double(quantity) * 2
+        
+        // comlicated cakes cost more
+        cost += (Double(type) / 2)
+        
+        // 1$ cake for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+        
+        return cost
+        
+    }
+    
+    init() { }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    
+}
